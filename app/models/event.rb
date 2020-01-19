aec0c8e316169495cca9d3b6a6a3de80fb575005
kajkal <karolesniak@gmail.com>
2020-01-19 21:55:56 +0100

    Dodanie walidacji modelu events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :artist, :presence => true
+  validates :price_low, :presence => true, numericality: {:greater_than => 0}
+  validates :price_high, :presence => true, numericality: {:greater_than => 0}
+  validates :event_date, :presence => true
 end
